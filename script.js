fca06aef41bee9fbe6215b61ed02949344895bf2
Md-arshath11 <arshath493@gmail.com>
2024-06-29 18:07:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var ul=document.getElementById("list-container")
+var input=document.getElementById("value")
+function add(){
+    var listitem=document.createElement("li")
+    listitem.innerHTML=input.value + "<button onclick='deleteItem(event)'>Delete</button>"
+    ul.append(listitem)
+}
+
+function deleteItem(event){
+    event.target.parentElement.remove()
+}
